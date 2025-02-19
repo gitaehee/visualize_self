753995778b12a6e6b1365bbe0938244f97e898a9
gitaehee <rainwifekth@naver.com>
2025-02-19 15:12:46 +0900

    fix: 모듈 에러 때문에 requirements.txt 스트림릿 폴더 안에도 추가

--- frontend/src/app/results/page.tsx
+++ frontend/src/app/results/page.tsx
@@ -74,6 +74,7 @@ export default function Results() {
       {/* ✅ Streamlit 결과를 불러와서 시각화 */}
       <iframe
         src="http://localhost:8501/?graph=bar"
+        //src="https://movie-visualize.streamlit.app/"
         style={{
           width: "100%",
           maxWidth: "100vw",
