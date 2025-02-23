9207dc2d4060221a8c5f884cc367bea84a3a44e3
gyooldata <kyuripark0424@gmail.com>
2025-02-23 15:13:13 +0900

    fix: base api 수정

--- frontend/src/const/baseApi.ts
+++ frontend/src/const/baseApi.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api";
+const baseApi = process.env.NEXT_PUBLIC_BASE_API || "http://localhost:3000";
+console.log("Base API URL:", baseApi);
