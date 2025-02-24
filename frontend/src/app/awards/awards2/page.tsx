17da8ba4b5d91d375482af021a55ce2ff75c905e
gitaehee <rainwifekth@naver.com>
2025-02-25 02:06:33 +0900

    feat: 어워즈 페이지 수정

--- frontend/src/app/awards/awards2/page.tsx
+++ frontend/src/app/awards/awards2/page.tsx
@@ -24,32 +24,12 @@ const messageStyle = css`
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
-const buttonStyle = css`
-  padding: 0.75rem 1.5rem;
-  background-color: #3b82f6;
-  color: white;
-  border-radius: 0.375rem;
-  transition: background-color 0.2s ease-in-out;
-  border: none;
-  cursor: pointer;
-  font-size: 1.2rem;
-  text-align: center;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3);
-
-  &:hover {
-    background-color: #2563eb;
-  }
-`;
 
 export default function Awards2() {
   const router = useRouter(); // 페이지 이동을 위한 useRouter
 
   return (
-    <div css={containerStyle}>
-      <h1 css={messageStyle}>222</h1>
-      <button css={buttonStyle} onClick={() => router.push("/awards/awards3")}>
-        다음
-      </button>
-    </div>
+    <div css={containerStyle} onClick={() => router.push("/awards/awards3")}>
+        </div>
   );
 }
---
+++ frontend/src/app/awards/awards6/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { css } from "@emotion/react";
+
+const containerStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/awardsimages/section6.svg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  color: white;
+`;
+
+const messageStyle = css`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1.5rem;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+`;
+
+export default function Awards6() {
+  const router = useRouter(); // 페이지 이동을 위한 useRouter
+
+  return (
+    <div css={containerStyle} onClick={() => router.push("/awards/awards7")}>
+    </div>
+  );
+}
